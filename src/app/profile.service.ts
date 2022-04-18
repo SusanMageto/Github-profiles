35bc73ecc802eaae802bfd4b10e5f1c3ae49714c
SusanMageto <susan.mageto@student.moringaschool.com>
2022-04-18 04:10:56 +0300

    calls the github api

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map'; 
+import 'rxjs/add/operator/map';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProfileService {
-
   private username!: string;
   clientid = '';
   clientsecret = '';
 
-
-  constructor(private http: HttpClient) { 
-    console.log ('service is now ready');
-    this.username= 'SusanMageto';
+  constructor(private http: HttpClient) {
+    console.log('service is now ready');
+    this.username = 'SusanMageto';
   }
 
-  getProfileInfo () {
-    
+  getProfileInfo() {
+    return this.http.get(
+      'https://api.github.com/users/' +
+        this.username +
+        '?client_id=' +
+        this.clientid +
+        '&clientsecret=' +
+        this.clientsecret
+    );
   }
 }
